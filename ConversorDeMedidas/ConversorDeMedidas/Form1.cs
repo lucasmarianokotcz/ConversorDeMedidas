8835f671160f64b714e7008eeb6c93d69ba08e5b
Alex <Alex@DESKTOP-PR075GD>
2019-08-31 22:00:12 -0300

    Código parcial

--- ConversorDeMedidas/ConversorDeMedidas/Form1.cs
+++ ConversorDeMedidas/ConversorDeMedidas/Form1.cs
@@ -16,5 +16,65 @@ namespace ConversorDeMedidas
         {
             InitializeComponent();
         }
+
+        Calcular calcular;
+
+        private void LimparTextBoxs()
+        {
+            foreach (Control ctl in pnlMedidas.Controls)
+            {
+                if (ctl is NumericUpDown)
+                {
+                    NumericUpDown num = ctl as NumericUpDown;
+                    num.Value = 0;
+                }
+            }
+        }
+
+        private void Calcular()
+        {
+            calcular = new Calcular();
+            bool input = false;
+
+            foreach (Control ctl in pnlMedidas.Controls)
+            {
+                if (ctl is NumericUpDown)
+                {
+                    NumericUpDown num = ctl as NumericUpDown;
+                    if (num.Value != 0)
+                    {
+                        if (input == false)
+                        {
+                            input = true; 
+                        }
+                        else
+                        {
+                            MessageBox.Show("Você só pode ter um valor definido para realizar o cálculo!", "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (input == false)
+            {
+                MessageBox.Show("")
+            }
+
+
+            if (numMedida1.Value != 0)
+            {
+                calcular.RecebePosição = "K";
+            }
+            else if (numMedida2.Value != 0)
+            {
+                calcular.RecebePosição = "H";
+            }
+            else if (numMedida3.Value != 0)
+            {
+                calcular.rece
+            }
+        }
+
     }
 }
